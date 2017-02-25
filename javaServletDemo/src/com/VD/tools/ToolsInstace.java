01dd61333992de539b78a440e7a85b06ba28e358
zhanggeng <m13716920635@163.com>
2017-02-25 09:51:05 +0800

    提交信息

--- javaServletDemo/src/com/VD/tools/ToolsInstace.java
+++ javaServletDemo/src/com/VD/tools/ToolsInstace.java
@@ -9,8 +9,18 @@ public class ToolsInstace<T>
     private static ToolsInstace obj;
     //private static Array<ToolsInstace<T>>
     private ToolsInstace()
-    
     {
+
+
+
+
+
+
+
+
+
+
+        
 
     }
 
